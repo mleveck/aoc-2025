35212206de4fad2d474cd5cd935d4ec928947ac6
Matt LeVeck <mleveck@gmail.com>
2025-12-07 16:07:13 -0800

    var naming

--- c/day07/p2.c
+++ c/day07/p2.c
@@ -4,21 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void swap(i64list* old, i64list* new) {
+void swap(i64list *old, i64list *new) {
     i64list temp = *old;
     *old = *new;
     *new = temp;
     memset(new->data, 0, sizeof(i64) * new->len);
 }
 
-u8 gets8(s8 str, usize idx) {
-    if (idx >= 0 && idx < str.len) {
-        return str.data[idx];
+u8 get_char(s8 line, usize idx) {
+    if (idx >= 0 && idx < line.len) {
+        return line.data[idx];
     }
     return '.';
 }
 
-void  set_s(i64list path_counts, s8 line) {
+void set_s(i64list path_counts, s8 line) {
     for (usize i = 0; i < line.len; i++) {
         if (line.data[i] == 'S') {
             path_counts.data[i] = 1;
@@ -30,30 +30,30 @@ void  set_s(i64list path_counts, s8 line) {
 }
 
 i64 process(s8list lines, arena scratch) {
+    usize nrows = lines.len;
     usize ncols = lines.data[0].len;
-    i64list prev_path_counts = {.data = new (&scratch, i64, ncols),
-                                .len = ncols};
+    i64list path_counts = {.data = new (&scratch, i64, ncols), .len = ncols};
     i64list new_path_counts = {.data = new (&scratch, i64, ncols),
                                .len = ncols};
-    set_s(prev_path_counts, lines.data[0]); // checked and S is on 1st line in
-                                            // sample input and actual input
-    for (usize i = 1; i < lines.len; i++) {
-        s8 line = lines.data[i];
-        for (usize col = 0; col < line.len; col++) {
+    set_s(path_counts, lines.data[0]); // checked and S is on 1st line in
+                                       // sample input and actual input
+    for (usize row = 1; row < nrows; row++) {
+        s8 line = lines.data[row];
+        for (usize col = 0; col < ncols; col++) {
             u8 c = line.data[col];
             if ('.' == c) {
-                new_path_counts.data[col] = prev_path_counts.data[col];
-                if (gets8(line, col - 1) == '^') {
-                    new_path_counts.data[col] += prev_path_counts.data[col - 1];
+                new_path_counts.data[col] = path_counts.data[col];
+                if ('^' == get_char(line, col - 1)) {
+                    new_path_counts.data[col] += path_counts.data[col - 1];
                 }
-                if (gets8(line, col + 1) == '^') {
-                    new_path_counts.data[col] += prev_path_counts.data[col + 1];
+                if ('^' == get_char(line, col + 1)) {
+                    new_path_counts.data[col] += path_counts.data[col + 1];
                 }
             }
         }
-        swap(&prev_path_counts, &new_path_counts);
+        swap(&path_counts, &new_path_counts);
     }
-    return sum(prev_path_counts);
+    return sum(path_counts);
 }
 
 int main(int argc, char **argv) {
