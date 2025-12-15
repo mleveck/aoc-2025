cbd9a3e31f4eb223651195faee36488cba6aae9e
= <matt.leveck@openlabsusa.org>
2025-12-14 21:34:51 -0800

    reorder arena functions

--- c/util.h
+++ c/util.h
@@ -45,6 +45,13 @@ typedef struct arena {
     u8 *end;
 } arena;
 
+static inline arena arena_create(size cap) {
+    arena a = {0};
+    a.beg = (u8 *)malloc(cap);
+    a.end = a.beg ? a.beg + cap : 0;
+    return a;
+}
+
 static inline void *alloc(arena *a, ptrdiff_t size, ptrdiff_t align,
                           ptrdiff_t count) {
     ptrdiff_t padding = -(uintptr_t)a->beg & (align - 1);
@@ -55,13 +62,6 @@ static inline void *alloc(arena *a, ptrdiff_t size, ptrdiff_t align,
     void *p = a->beg + padding;
     a->beg += padding + count * size;
     return memset(p, 0, count * size);
-}
-
-static inline arena arena_create(size cap) {
-    arena a = {0};
-    a.beg = (u8 *)malloc(cap);
-    a.end = a.beg ? a.beg + cap : 0;
-    return a;
 }
 
 #define new(a, t, n) (t *)alloc(a, sizeof(t), _Alignof(t), n)
